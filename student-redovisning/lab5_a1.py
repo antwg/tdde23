d8ee37dd2ec4332566d37c592b758f0964a3738f
antwe841 <antwe841@su12-106.ad.liu.se>
2020-09-30 10:46:48 +0200

    Final

--- student-redovisning/lab5_a1.py
+++ student-redovisning/lab5_a1.py
@@ -1,76 +1,13 @@
 import cv2
-def vcimg_to_list(img):
+def cvimg_to_list(img):
+    """Takes an OpenCV picture and returns a list with BGR tuples"""
     pixlar = []
     rader = img.shape[0]
     kolumn = img.shape[1]
+
     for x in range(rader):
         for y in range(kolumn):
             pixel = img[x, y]
             pixlar.append((pixel[0], pixel[1], pixel[2]))
+
     return pixlar
-
-
-"""cvlib module, helper functions for image and tuple manipulation."""
-
-import cv2
-import numpy
-
-# ------------------------------------------
-#  Helper functions
-# ------------------------------------------
-
-
-def multiply_tuple(tpl, mult):
-    """Return a tuple where all elements are scaled by factor 'mult'.
-
-    (a,b,c) * k = (a*k, b*k, c*k)
-    """
-    return tuple(map(lambda x: x*mult, tpl))
-
-
-def add_tuples(tpl1, tpl2):
-    """
-    Return a the element-wise sum of tpl1 and tpl2.
-
-    (a,b,c) + (d,e,f) = (a+d, b+e, c+f)
-    """
-    return tuple(map(lambda t1, t2: t1+t2, tpl1, tpl2))
-
-
-# -------------------------------------------
-#  Converting between python list and images
-# -------------------------------------------
-
-
-def rgblist_to_cvimg(lst, height, width):
-    """Return a width x height OpenCV image with specified pixels."""
-    # A 3d array that will contain the image data
-    img = numpy.zeros((height, width, 3), numpy.uint8)
-
-    for x in range(0, width):
-        for y in range(0, height):
-            pixel = lst[y * width + x]
-            img[y, x, 0] = pixel[0]
-            img[y, x, 1] = pixel[1]
-            img[y, x, 2] = pixel[2]
-
-    return img
-
-
-def greyscale_list_to_cvimg(lst, height, width):
-    """Return a width x height grayscale OpenCV image with specified pixels."""
-    img = numpy.zeros((height, width), numpy.uint8)
-
-    for x in range(0, width):
-        for y in range(0, height):
-            img[y, x] = lst[y * width + x]
-
-    return img
-
-def test():
-    img = cv2.imread('image.jpg')
-    list_img = cvimg_to_list(img)
-    converted_img = rgblist_to_cvimg(list_img, img.shape[0], img.shape[1])    # Bildens dimensioner
-    cv2.imshow("converted", converted_img)
-
-    cv2.waitKey(0)
