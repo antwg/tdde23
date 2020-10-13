779473782e61af1f33c40fef2945d062e7b89009
antwe841 <antwe841@tlhw-5-4.ad.liu.se>
2020-10-13 11:24:28 +0200

    Testfunktion för generate_from_image

--- student-utveckling/uppgift5c.py
+++ student-utveckling/uppgift5c.py
@@ -46,46 +46,25 @@ def test_pixel_constraint():
     #assert(greyscale_to_tuple(sky_pixels) == sky_pixels_result_list)
 
 
-"""
 def test_generator_from_image():
-    #Test for generator_from_image
-    orig_img = cv2.imread("plane.jpg")
-    orig_list = cvimg_to_list(orig_img)
-
-    generator = generator_from_image(orig_list)
-
-    new_list = [generator(i) for i in range(len(orig_list))]
-
-    cv2.imshow('new', rgblist_to_cvimg(new_list, orig_img.shape[0], orig_img.shape[1]))
-    cv2.waitKey(0)
+    test_if_function()
+    test_pixel_rgb_values([(0,0,0), (0,128,255), (255,0,128)], 0, 0, 0, 0)
+    test_pixel_rgb_values([(0,0,0), (0,128,255), (255,0,128)], 1, 0, 128, 255)
+    test_pixel_rgb_values([(0,0,0), (0,128,255), (255,0,128)], 2, 255, 0, 128)
+    print('Passed all tests')
 
 
-    def test_combine_images():
-        #Test for combine images
+def test_if_function():
+    """Tests if a function returns a function"""
+    var = generator_from_image('list')
+    assert(callable(var) == True)
 
-        # Läs in en bild
-        plane_img = cv2.imread("plane.jpg")
 
-        # Skapa ett filter som identifierar himlen
-        condition = pixel_constraint(100, 150, 50, 200, 100, 255)
+def test_pixel_rgb_values(image_list, pixel, r, g, b):
+    """Tests if function returns the correct rgb values of a pixel"""
+    func = generator_from_image(image_list)
+    assert(func(pixel) == (r, g, b))
 
-        # Omvandla originalbilden till en lista med HSV-fä?rger
-        hsv_list = cvimg_to_list(cv2.cvtColor(plane_img, cv2.COLOR_BGR2HSV))
-        plane_img_list = cvimg_to_list(plane_img)
 
-        # Skapa en generator som gör en stjärnhimmel
-        def generator1(index):
-            val = random.random() * 255 if random.random() > 0.99 else 0
-            return (val, val, val)
-
-        # Skapa en generator fö?r den inlä?sta bilden
-        generator2 = generator_from_image(plane_img_list)
-
-        # Kombinera de två bilderna till en, alltså anvä?nd himmelsfiltret som mask
-        result = combine_images(hsv_list, condition, generator1, generator2)
-
-            # Omvandla resultatet till en riktig bild och visa upp den
-        new_img = rgblist_to_cvimg(result, plane_img.shape[0], plane_img.shape[1])
-        cv2.imshow('Final image', new_img)
-        cv2.waitKey(0)
-"""
+def test_combine_images():
+    pass
